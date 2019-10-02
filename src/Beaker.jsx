08d3b0790227f9b6e99a83e8fce3bffa4373a790
Cavanaugh Richards <cavatron@pm.me>
2019-10-01 19:29:13 -0500

    add style to beaker component

--- src/Beaker.jsx
+++ src/Beaker.jsx
@@ -1,22 +1,51 @@
 import React from 'react';
 import { useService } from '@xstate/react';
 
+import { Badge, Button, Container, Row, Col } from 'reactstrap';
+
 export default function ({ actorRef }) {
   const [state, send] = useService(actorRef);
 
   const { water, scoops } = state.context;
 
   return (
-    <>
-      <div>Water: {water}, Scoops: {scoops}</div>
-      {state.matches('unstirred') && <div>Unstirred</div>}
-      {state.matches('stirred') && <div>Stirred</div>}
 
-      <button disabled={state.matches('filling.water.full') || state.matches('unstirred') || state.matches('stirred')} onClick={() => send('ADD_WATER')}>Add water</button>
+    <Container>
+      <Row>
+        <Col>
+          <p>Water: {water} mL</p>
+        </Col>
+        <Col>
+          <p>Coffee scoops: {scoops}</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          {state.matches('unstirred') && <Badge>Unstirred</Badge>}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
 
-      <button disabled={state.matches('filling.scoop.full') || state.matches('unstirred') || state.matches('stirred')} onClick={() => send('ADD_SCOOP')}>Add scoop</button>
+          <Button disabled={state.matches('filling.water.full') || state.matches('unstirred') || state.matches('stirred')} onClick={() => send('ADD_WATER')}>Add water</Button>
+        </Col>
+        <Col>
 
-      <button disabled={state.matches('empty') || state.matches('stirred') || state.matches('filling.water.filling') || state.matches('filling.scoop.filling')} onClick={() => send('STIR')}>Stir</button>
-    </>
+
+          <Button disabled={state.matches('filling.scoop.full') || state.matches('unstirred') || state.matches('stirred')} onClick={() => send('ADD_SCOOP')}>Add scoop</Button>
+        </Col>
+        <Col>
+
+          <Button disabled={state.matches('empty') || state.matches('stirred') || state.matches('filling.water.filling') || state.matches('filling.scoop.filling')} onClick={() => send('STIR')}>Stir</Button>
+        </Col>
+      </Row>
+    </Container>
+
+
+
+
+
+
+
   );
 }
